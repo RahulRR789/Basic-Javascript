2d64ebbced4f96159bc7f1c8648e3c62f142cf8b
RahulRR789 <rahul7892rajput@gmail.com>
2024-11-11 20:11:51 +0530

    New Javascript

---
+++ Day1/script.js
@@ -0,0 +1,82 @@
+// Variables
+// Declarations of var
+var a ;
+
+// initialization
+a=20;
+console.log(a);
+
+// decalartion and intialization in same line
+
+var a = 30;
+console.log(a);
+
+// Re-decalartion
+var a ;
+
+// re-intialization 
+a = 35;
+console.log(a);
+
+// re-decalartion and re-intialization in same line
+var a= 39;
+console.log(a);
+
+
+// variables 
+//  decalartion of let 
+
+let b;
+
+// intialization 
+b=100;
+console.log(b);
+
+//  decalartion and intialization in same line
+
+// let b =100;
+// console.log(b);
+
+//  re-decalartion
+// let b;
+// Uncaught SyntaxError: Identifier 'b' has already been declared
+
+// re-intialization 
+b=203;
+console.log(b);
+
+// re-decalartion and re-intialization in same line
+// let b =1000;
+// console.log(b);
+// Uncaught SyntaxError: Identifier 'b' has already been declared
+
+// variables
+// decalartion of const
+// const c;
+// Uncaught SyntaxError: Missing initializer in const declaration
+
+// intialization 
+// c = 1200;
+// console.log(c);
+// initialization is not possible 
+
+//  decalartion and intialization in same line
+const c = 1200;
+console.log(c);
+
+//  re-decalartion
+// const c;
+// Uncaught SyntaxError: Identifier 'c' has already been declared
+
+// re-intialization 
+// c =1200;
+// console.log(c);
+// Uncaught TypeError: Assignment to constant variable at 
+
+// re-decalartion and re-intialization in same line
+
+// const c =12000;
+// console.log(c);
+//Uncaught SyntaxError: Identifier 'c' has already been declared
+
+
---
+++ script.js
@@ -0,0 +1,28 @@
+// Variables
+// Declarations of var
+var a ;
+
+// initialization
+a=20;
+console.log(a);
+
+// decalartion and intialization in same line
+
+var a = 30;
+console.log(a);
+
+// Re-decalartion
+var a ;
+
+// re-intialization 
+a = 35;
+console.log(a);
+
+// re-decalartion and re-intialization in same line
+var a= 39;
+console.log(a);
+
+
+
+
+
