6e625ec925a0de05d7e3457d08488b1ff9e721d1
RahulRR789 <rahul7892rajput@gmail.com>
2024-11-12 22:03:17 +0530

    Const Concept

--- script.js
+++ script.js
@@ -49,6 +49,35 @@ let b =1000;
  console.log(b);
 // Uncaught SyntaxError: Identifier 'b' has already been declared
 
+// variables
+// decalartion of const
+// const c;
+// Uncaught SyntaxError: Missing initializer in const declaration
+
+// intialization 
+// c = 1200;
+// console.log(c);
+// initialization is not possible 
+
+//  decalartion and intialization in same line
+const c = 1200;
+console.log(c);
+
+//  re-decalartion
+// const c;
+// Uncaught SyntaxError: Identifier 'c' has already been declared
+
+// re-intialization 
+// c =1200;
+// console.log(c);
+// Uncaught TypeError: Assignment to constant variable at 
+
+// re-decalartion and re-intialization in same line
+
+// const c =12000;
+// console.log(c);
+//Uncaught SyntaxError: Identifier 'c' has already been declared
+
 
 
 
